2bb3d7464b2f48debab0872d5d23ea11c87572d1
LUIS OTERO GOMEZ <100472104@alumnos.uc3m.es>
2025-10-30 16:19:19 +0100

    Modify styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ClassDiagram from "./ClassDiagram";
 import SideMenu from "./SideMenu";
 
+import "./ClassDiagram.css";
+
 function App() {
     return (
         <div style={{ width: "100%", height: "100vh" }}>
